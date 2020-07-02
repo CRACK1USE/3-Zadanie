da1039b8bbacceecf1a8b322d770708698ba809e
Антон Cурков <toni.surkov.17@mail.ru>
2020-07-02 19:30:42 +0400

    Добавьте файлы проекта.

---
+++ 3z/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3z
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите p: ");
+            float p = float.Parse(Console.ReadLine());
+
+            double x = n * p;
+
+            double y = Math.Pow(x, 3) / 6 * Math.Pow(2.71828, -0.1);
+            y = Math.Round(y, 4);
+            Console.WriteLine("Вероятность: " + y);
+            Console.ReadKey();
+        }
+    }
+}
